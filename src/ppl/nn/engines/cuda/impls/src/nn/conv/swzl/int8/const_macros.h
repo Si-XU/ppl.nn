be5cc01a1496ecba464e6ae5cabc019413c36f30
liuyuxi <liuyuxi@sensetime.com>
2022-05-30 10:57:08 +0800

    [cuda][jit] add windows macros for int8 conv kernels

--- src/ppl/nn/engines/cuda/impls/src/nn/conv/swzl/int8/const_macros.h
+++ src/ppl/nn/engines/cuda/impls/src/nn/conv/swzl/int8/const_macros.h
@@ -15,6 +15,31 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#if defined(_WIN64) || defined(_WIN32)
+    #define uint unsigned int
+
+#ifdef _MSC_VER
+    #if _MSC_VER >= 1600
+    #include <cstdint>
+    #else
+    typedef __int8 int8_t;
+    typedef __int16 int16_t;
+    typedef __int32 int32_t;
+    typedef __int64 int64_t;
+    typedef unsigned __int8 uint8_t;
+    typedef unsigned __int16 uint16_t;
+    typedef unsigned __int32 uint32_t;
+    typedef unsigned __int64 uint64_t;
+    #endif
+    #endif
+#endif
+
+#ifndef PPLNN_ENABLE_CUDA_JIT
+#if (defined(_WIN64) || defined(_WIN32))
+    #define _Pragma __pragma
+#endif
+#endif
+
 ////////////////////////////////////////
 // kernel list macros
 ////////////////////////////////////////
